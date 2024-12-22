7b43d1e22775f7f34e8ec775a6fed9efd9a8d92a
Zua <gimzua0228@gmail.com>
2024-12-22 17:36:56 +0900

    logging 추가

--- src/main/java/com/dev/snsboard/controller/PostController.java
+++ src/main/java/com/dev/snsboard/controller/PostController.java
@@ -4,6 +4,8 @@ import com.dev.snsboard.model.Post;
 import com.dev.snsboard.model.PostPatchRequestBody;
 import com.dev.snsboard.model.PostPostRequestBody;
 import com.dev.snsboard.service.PostService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,24 +16,29 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/v1/posts")
 public class PostController {
+
+    private static final Logger logger = LoggerFactory.getLogger(PostController.class);
     
     @Autowired 
     private PostService postService;
 
     @GetMapping
     public ResponseEntity<List<Post>> getPosts() {
+        logger.info("GET /api/v1/posts");
         var posts = postService.getPosts();
         return ResponseEntity.ok(posts);
     }
 
     @GetMapping("/{postId}")
     public ResponseEntity<Post> getPostByPostId(@PathVariable Long postId) {
+        logger.info("GET /api/v1/posts/{}", postId);
         var post = postService.getPostByPostId(postId);
         return ResponseEntity.ok(post);
     }
 
     @PostMapping
     public ResponseEntity<Post> createPost(@RequestBody PostPostRequestBody postPostRequestBody) {
+        logger.info("POST /api/v1/posts");
         var post = postService.createPost(postPostRequestBody);
         return ResponseEntity.ok(post);
     }
@@ -39,12 +46,14 @@ public class PostController {
     @PatchMapping("/{postId}")
     public ResponseEntity<Post> updatePost(
             @PathVariable Long postId, @RequestBody PostPatchRequestBody postPatchRequestBody) {
+        logger.info("PATCH /api/v1/posts/{}", postId);
         var post = postService.updatePost(postId, postPatchRequestBody);
         return ResponseEntity.ok(post);
     }
 
     @DeleteMapping("/{postId}")
     public ResponseEntity<Void> deletePost(@PathVariable Long postId) {
+        logger.info("DELETE /api/v1/posts/{}", postId);
         postService.deletePost(postId);
         // 204 No Content
         return ResponseEntity.noContent().build();
