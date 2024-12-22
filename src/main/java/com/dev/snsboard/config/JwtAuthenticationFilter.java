c1cba696288b83f631c8b5f2859d95ddb94691b0
Zua <gimzua0228@gmail.com>
2024-12-22 20:41:11 +0900

    JwtAuthenticationFilter 작성

---
+++ src/main/java/com/dev/snsboard/config/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package com.dev.snsboard.config;
+
+import com.dev.snsboard.service.JwtService;
+import com.dev.snsboard.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserService userService;
+
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String BEARER_PREFIX = "Bearer ";
+        // request 로 부터 head 정보에 접근해서 AUTHORIZATION 값을 추출해 낸다.
+        var authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+        var securityContext = SecurityContextHolder.getContext();
+
+           // authorization 비어 있지 않은 경우 & authorization 이 BEARER_PREFIX 로 시작하는 경우 JWT 인증 시작
+        if (!ObjectUtils.isEmpty(authorization)
+            && authorization.startsWith(BEARER_PREFIX)
+            && securityContext.getAuthentication() == null) {
+
+            var accessToken = authorization.substring(BEARER_PREFIX.length());
+            var username = jwtService.getUsername(accessToken);
+            var userDetails = userService.loadUserByUsername(username);
+
+            var authenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.getAuthorities());
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            securityContext.setAuthentication(authenticationToken);
+            SecurityContextHolder.setContext(securityContext);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
